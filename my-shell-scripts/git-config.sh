88572000ff1b8305be76eae0e4c199cc2a1b1f16
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-05 10:06:36 -0400

    Completed 42-monsters-rolodex-optimizations

---
+++ my-shell-scripts/git-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+# I lost an hour of work due to not running this command before starting work.
+
+git config --global user.name "goldmouthdogg88"
+git config --global user.email goldmouthdogg88@gmail.com
+
+
+git config --list | grep "goldmouthdogg88"
